528c04098c674f45a6bd2644df7fd1df96dcc8c8
PandaMan16 <contact@pandatown.fr>
2023-06-12 09:47:22 +0200

    bugged pagination swipper

--- main.js
+++ main.js
@@ -9,9 +9,46 @@ loader.querySelector(".content").appendChild(bar);
 // panda.timeaction.add(bar,{"list":[{"action":"style","value":"width:100%;position:relative;top:&1%;left:&1vw","init":0,"add":-7}],"start":7,"end":9});
 // panda.timeaction.add(bar,{"list":[{"action":"style","value":"width:100%;position:relative;top:&1%;left:&1vw","init":-14,"add":7}],"start":10,"end":12});
 // panda.timeaction.add(loader,{"list":[{"action":"style","value":"opacity:&1","init":1,"add":-1}],"start":10,"end":11});
-panda.timeaction.add(loader,{"list":[{"action":"style","value":"opacity:&1","init":0,"add":0}],"start":0,"end":1});
-panda.timeaction.add(loader,{"list":[{"action":"style","value":"display:none","init":0,"add":0}],"start":0,"end":1});
+panda.timeaction.add(document.querySelector("#main"),{"list":[{"action":"style","value":"display:none","init":0,"add":0}],"start":0,"end":1});
+panda.timeaction.add(loader,{"list":[{"action":"style","value":"opacity:&1","init":0,"add":0}],"start":1,"end":2});
+panda.timeaction.add(loader,{"list":[{"action":"style","value":"display:none","init":0,"add":0}],"start":1,"end":2});
+panda.timeaction.add(document.querySelector("#main"),{"list":[{"action":"style","value":"display:","init":0,"add":0}],"start":1,"end":2});
 
+var swiper = new Swiper('.swiper', {
+    slidesPerView: "auto",
+    spaceBetween: 15,
+    // breakpoints: {
+    //   640: {
+    //     slidesPerView: 3,
+    //     spaceBetween: 5,
+    //   },
+    //   1024: {
+    //     slidesPerView: 7,
+    //     spaceBetween: 15,
+    //   }
+    // },
+    //direction: getDirection(),
+    mousewheel: {
+        releaseOnEdges: false,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+      hiddenClass: '.hidden',
+    },
+    mousewheel: {
+      forceToAxis: true,
+    },
+    on: {
+        // resize: function () {
+        //   swiper.changeDirection(getDirection());
+        // },
+    },
+  });
 
+  function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
 
-
+    return direction;
+  }
