68119b34727a076977a33bb3f73273e400198dc6
PandaMan16 <contact@pandatown.fr>
2023-06-18 01:08:54 +0200

    fix bug and ready for beta

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { panda } from "https://pandatown.fr/lib/pandalib.php";
+import { panda } from "./pandalib.js";
 // import { panda } from "http://lib.panda/pandalib.js";
 
 let loader = document.querySelector("#loader");
@@ -12,6 +12,7 @@ loader.querySelector(".content").appendChild(bar);
 // panda.timeaction.add(bar,{"list":[{"action":"style","value":"width:100%;position:relative;top:&1%;left:&1vw","init":-14,"add":7}],"start":10,"end":12});
 // panda.timeaction.add(loader,{"list":[{"action":"style","value":"opacity:&1","init":1,"add":-1}],"start":10,"end":11});
 panda.timeaction.add(document.querySelector("#main"),{"list":[{"action":"style","value":"display:none","init":0,"add":0}],"start":0,"end":1});
+
 const game = {
   baseimg:"https://image.tmdb.org/t/p/w300_and_h450_bestv2",
   baseimg2:"https://image.tmdb.org/t/p/w1920_and_h1080_bestv2",
@@ -24,7 +25,7 @@ const game = {
       document.querySelector("#saisie").value = "";
       game.saisie(saisie);
     });
-    const save = panda.cookie.read("moviequiz");
+    const save = panda.cookie.read("moviequizF");
     const FilmN = document.querySelector("#FilmsN .swiper-wrapper");
     const SeriesN = document.querySelector("#SeriesN .swiper-wrapper");
     const FilmT = document.querySelector("#FilmsT .swiper-wrapper");
@@ -35,71 +36,95 @@ const game = {
           return json;
       }
     );
-    console.log("save",save);
     if(save){
-      this.game = save;
+      this.game = {
+        list:{movies:[],series:[]},
+        found:save
+      }
     }else{
       this.game = {
-        movies:[],
-        series:[],
-        found:{movie:[],serie:[]},
+        list:{movies:[],series:[]},
+        found:{movie:[],serie:[]}
       };
     }
     
     for (const key in jsonfile.medias.movie) {
       const info = await this.getinfo(jsonfile.medias.movie[key].title,"movie");
-      info.type = "movie";
-      if(info.backdrop_path){
-        this.game.movies.push(info);
+      
+      if(info.backdrop_path && this.game.found.movie.findIndex((movie) => movie.id == info.id) == -1){
+        let data = {"type":"movie","id":info.id,"title":info.title.split(" : ")[0],"poster_path":info.poster_path,"backdrop_path":info.backdrop_path};
+        this.game.list.movies.push(data);
         let image = this.baseimg+info.backdrop_path;
         const coverelem = panda.util.newelem("div",{"className":"swiper-slide","style":"background-image: url('"+image.replace(".jpg",".jpg")+"');"});
-        coverelem.id = info.type+info.id;
+        coverelem.id = data.type+data.id;
         FilmN.appendChild(coverelem);
         coverelem.addEventListener("click",function(e){
-          game.select(info);
+          game.select(data);
           // panda.util.log(jsonfile.medias.movie[key].title);
         });
       }else{
-        panda.util.log("aucune image disponible : "+jsonfile.medias.movie[key].title);
+        if(!info.backdrop_path){
+          panda.util.log("aucune image disponible : "+jsonfile.medias.movie[key].title,"red");
+        }
       }
     }
     for (const key in jsonfile.medias.serie) {
       const info = await this.getinfo(jsonfile.medias.serie[key].title,"tv");
-      info.type = "tv";
-      if(info.backdrop_path){
-        this.game.series.push(info);
+      if(info.backdrop_path && this.game.found.serie.findIndex((serie) => serie.id == info.id) == -1){
+        let data = {"type":"tv","id":info.id,"name":info.name.split(" : ")[0],"poster_path":info.poster_path,"backdrop_path":info.backdrop_path};
+        this.game.list.series.push(data);
         let image = this.baseimg+info.backdrop_path;
         const coverelem = panda.util.newelem("div",{"className":"swiper-slide","style":"background-image: url('"+image.replace(".jpg",".jpg")+"');"});
-        coverelem.id = info.type+info.id;
+        coverelem.id = data.type+data.id;
         SeriesN.appendChild(coverelem);
         coverelem.addEventListener("click",function(e){
-          game.select(info);
+          game.select(data);
           // panda.util.log(jsonfile.medias.movie[key].title);
         });
       }else{
-        panda.util.log("aucune image disponible : "+jsonfile.medias.serie[key].title);
+        if(!info.backdrop_path){
+          panda.util.log("aucune image disponible : "+jsonfile.medias.movie[key].title,"red");
+        }
       }
     }
     FilmT.appendChild(panda.util.newelem("div",{"className":"swiper-slide","id":"FilmEmpty"}));
     SeriesT.appendChild(panda.util.newelem("div",{"className":"swiper-slide","id":"SerieEmpty"}));
 
-    if(this.game.found.movie > 0){
+    if(this.game.found.movie.length > 0){
       document.querySelector("#FilmEmpty").style.display = "none";
       for (const key in this.game.found.movie) {
         let image = this.baseimg+this.game.found.movie[key].poster_path;
         const coverelem = panda.util.newelem("div",{"className":"swiper-slide","style":"background-image: url('"+image.replace(".jpg",".jpg")+"');"});
-        coverelem.dataset.id = this.selected.type+this.selected.id;
+        coverelem.id = this.game.found.movie[key].type+this.game.found.movie[key].id;
         FilmT.appendChild(coverelem);
       }
     }
-    if(this.game.found.serie > 0){
+    if(this.game.found.serie.length > 0){
       document.querySelector("#SerieEmpty").style.display = "none";
       for (const key in this.game.found.serie) {
         let image = this.baseimg+this.game.found.serie[key].poster_path;
         const coverelem = panda.util.newelem("div",{"className":"swiper-slide","style":"background-image: url('"+image.replace(".jpg",".jpg")+"');"});
-        coverelem.dataset.id = this.selected.type+this.selected.id;
+        coverelem.id = this.game.found.serie[key].type+this.game.found.serie[key].id;
         SeriesT.appendChild(coverelem);
       }
+    }
+    if(this.game.found.movie.length+this.game.found.serie.length != 0){
+      let rdm = panda.util.rdm(1,this.game.found.movie.length+this.game.found.serie.length);
+      let select = null;
+      if(this.game.found.movie.length >= rdm){
+        select = this.game.found.movie[rdm-1];
+      }else{ 
+        select = this.game.found.serie[rdm-this.game.found.movie.length-1];
+      };
+      let head = document.querySelector(".acceuil > .head");
+      head.style.display = "";
+      if(select.type == "movie"){
+        head.querySelector("h1").innerHTML = select.title;
+      }else if(select.type == "tv"){
+        head.querySelector("h1").innerHTML = select.name;
+      }
+      document.querySelector(".background > div").style.backgroundImage = `url(${this.baseimg2+select.backdrop_path})`;  
+      document.querySelector(".background > div").style.filter = "grayscale(0)";
     }
   },
   getinfo:async function(title,type){
@@ -115,7 +140,12 @@ const game = {
       return result;
   },
   select:function(info){
+    if(this.interval){
+      clearInterval(this.interval);
+    }
+    document.querySelector(".acceuil > .head").style.display = "none";
     document.querySelector(".background > div").style.backgroundImage = `url(${this.baseimg2+info.backdrop_path})`;
+    document.querySelector(".background > div").style.filter = "grayscale(1)";
     document.querySelector("#saisieUser").style.display = "";
     this.selected = info;
   },
@@ -127,7 +157,6 @@ const game = {
       }else if(this.selected.type == "tv"){
         percent = this.matchPourcentage(saisie,this.selected.name);
       }
-      panda.util.log(percent,"orange");
       const reponse = document.querySelector("#resultSaisie");
       if(percent >= 75){
         reponse.style.display = "";
@@ -135,8 +164,8 @@ const game = {
 
         switch(this.selected.type){
           case "movie":
-            let i = this.game.movies.findIndex((movie) => movie.title == this.selected.title);
-            this.game.movies.splice(i,1);
+            let i = this.game.list.movies.findIndex((movie) => movie.title == this.selected.title);
+            this.game.list.movies.splice(i,1);
             this.game.found.movie.push(this.selected);
             let newfoundM = panda.util.newelem("div",{"className":"swiper-slide","style":"background-image: url('"+this.baseimg+this.selected.poster_path+"'"});
             document.querySelector("#FilmsT .swiper-wrapper").appendChild(newfoundM);
@@ -144,8 +173,8 @@ const game = {
             document.querySelector("#FilmEmpty").style.display = "none";
             break;
           case "tv":
-            let s = this.game.series.findIndex((serie) => serie.title == this.selected.title);
-            this.game.series.splice(s,1);
+            let s = this.game.list.series.findIndex((serie) => serie.title == this.selected.title);
+            this.game.list.series.splice(s,1);
             this.game.found.serie.push(this.selected);
             let newfoundS = panda.util.newelem("div",{"className":"swiper-slide","style":"background-image: url('"+this.baseimg+this.selected.poster_path+"'"});
             document.querySelector("#SeriesT .swiper-wrapper").appendChild(newfoundS);
@@ -154,13 +183,18 @@ const game = {
             break;
           default:
             return;
-        }        
-        panda.cookie.save(this.game,"moviequiz");
+        }
+        document.querySelector(".background > div").style.backgroundImage = `url(${this.baseimg2+this.selected.backdrop_path})`;
+        document.querySelector(".background > div").style.filter = "grayscale(0)";
+        this.interval = setInterval(() => {
+          document.querySelector("#saisieUser").style.display = "none";
+        },5000);
+        panda.cookie.save(game.game.found,"moviequizF");
       }else{
         reponse.style.display = "";
         reponse.innerHTML = `<p>MAUVAISE RÉPONSE</p>`;
-
       }
+      setInterval(()=>{reponse.style.display = "none";},5000);
     }
   },
   clear:function(){
